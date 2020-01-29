1f257d2cf58cf404af7b25d76b3a0b498ed7d840
kellycoles <kellycoles1@gmail.com>
2020-01-29 12:36:54 -0600

    reverse string with reverse method

--- codeChallenges.js
+++ codeChallenges.js
@@ -7,6 +7,12 @@ for(let i = str.length - 1; i >= 0; i--)
 {
 reverse += str. charAt(i);
 }
-console.log("Reversed string is:");
+console.log("The reversed string is:");
 console.log(reverse);
-alert("what?")
+
+//with reverse method
+//=============================================
+let string  = ['kelly', 'Coles']
+let newstr = string.reverse();
+console.log("The reversed string method string is:");
+console.log(newstr);
