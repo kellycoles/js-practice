112bb1a698c4513fe0ba50b7c2ed8b66e2765d19
kellycoles <kellycoles1@gmail.com>
2020-01-29 14:41:25 -0600

    fibonacci

--- codeChallenges.js
+++ codeChallenges.js
@@ -31,7 +31,7 @@ const palindrome = str => {
  let result = palindrome ('table');
  console.log(result);
 
- //count the vowelsin a string
+ //count the vowels in a string
  //======================================================
  const findVowels = str => {
     let count = 0
@@ -43,4 +43,28 @@ const palindrome = str => {
     }
     console.log(count);
   }
-  findVowels("kelly")
+  findVowels("apple")
+
+  // fibonacci
+  //======================================================
+  const fibonacci = num => {
+    //     // store the Fibonacci sequence you're going
+    //     // to generate inside an array and
+    //     // initialize the array with the first two
+    //     // numbers of the sequence
+         const result = [0, 1];
+      
+         for(let i = 2; i <= num; i++) {
+    //       // push the sum of the two numbers
+    //       // preceding the position of i in the result array
+    //       // at the end of the result array
+           const prevNum1 = result[i - 1]
+           const prevNum2 = result[i - 2]
+           result.push(prevNum1 + prevNum2)
+        }
+        // return the last value in the result array
+        console.log(result[num]);
+      }
+      fibonacci(6)
+
+      
