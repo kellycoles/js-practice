25e5d6ac9b700bd4aa689c99caaa44e168e403c5
kellycoles <kellycoles1@gmail.com>
2020-01-29 12:30:50 -0600

    reverse string w/o reverse method

---
+++ codeChallenges.js
@@ -0,0 +1,12 @@
+// string char reverse without reverse method
+//=============================================
+
+let reverse = ""
+let str  = 'kelly'
+for(let i = str.length - 1; i >= 0; i--)
+{
+reverse += str. charAt(i);
+}
+console.log("Reversed string is:");
+console.log(reverse);
+alert("what?")
