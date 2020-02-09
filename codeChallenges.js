02b25865d072ff5d8255ebd09cd022cef92cd2af
kellycoles <kellycoles1@gmail.com>
2020-02-08 23:04:12 -0600

    chess board

--- codeChallenges.js
+++ codeChallenges.js
@@ -16,24 +16,24 @@ let newstr = string.reverse();
 console.log("The reversed string method string is:");
 console.log(newstr); // returns[coles, kelly]
 
-// //reverse string with split/reverse/join
-// //===================================================================
-// function reverseString(str) {
-//     // Step 1. Use the split() method to return a new array
-//     var splitString = str.split(""); // var splitString = "hello".split("");
-//     // ["h", "e", "l", "l", "o"]
+//reverse string with split/reverse/join
+//===================================================================
+function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
 
-//     // Step 2. Use the reverse() method to reverse the new created array
-//     var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
-//     // ["o", "l", "l", "e", "h"]
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
 
-//     // Step 3. Use the join() method to join all elements of the array into a string
-//     var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
-//     // "olleh"
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
 
-//     //Step 4. Return the reversed string
-//     return joinArray; // "olleh"
-// }
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
 
 // reverseString("hello");
 
@@ -128,3 +128,21 @@ const fizzBuzz = num => {
     }
 }
 fizzBuzz(40);
+//===========================================================
+// chess board
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
